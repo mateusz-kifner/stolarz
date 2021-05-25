b4901d36de512d072e1a98d7acb737408e9cec83
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2021-05-25 13:30:28 +0200

    npm upgrade(manual)

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import "fontsource-roboto"
+import "@fontsource/roboto"
 import "./index.css"
 
 ReactDOM.render(
